75a256e5c6231d72f436303b05c833a7d7f0cec0
Sonii <thomasremond@yahoo.fr>
2014-02-14 19:08:17 +0100

    Correction of trainAndTest bash script

--- trainTestAndTellMe.sh
+++ trainTestAndTellMe.sh
@@ -2,10 +2,10 @@
 
 if test -z "$1" 
 then
-     echo "Veuillez entrer le nombre de neuronnes cachées souhaités"
+     echo "Veuillez entrer le nombre de neuronnes cachées souhaités en arguments [3 - 100]"
+     exit  
 fi
 
-nbrNeurons = $1
+nbrNeurons=$1
 ./bestTrain trainDataSet.txt testDataSet.txt neuronsDataTrained $nbrNeurons
-./test.exe testDataSet neuronsDataTrained_$nbrNeurons.net > prediction.txt
-./confusionMatrix.pl < prediction.txt > stats_$nbrNeurons.txt
+./test.exe testDataSet neuronsDataTrained_$nbrNeurons.net | ./confusionMatrix.pl > stats_$nbrNeurons.txt
